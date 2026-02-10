b74e8f0cb87cf6c86f27cff5b7cffd9ae64d9639
David Gonzalez Casin <david.gonzalezcasin@gmail.com>
2026-02-10 21:51:40 +0100

    chore: add Vercel analytics and speed insights

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,12 @@
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
+import { SpeedInsights } from '@vercel/speed-insights/nuxt'
+</script>
+
 <template>
   <UApp>
+    <Analytics />
+    <SpeedInsights />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
